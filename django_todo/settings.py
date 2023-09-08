2010b241e0658318855feb500c17ed999e0b542d
Indrakens <>
2023-09-08 19:16:40 +0000

    removed secret key

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
-    'SECRET_KEY', 'django-insecure-u&#^trsx14qbrji8mouj7uzh&r1@iwb1i2w2j^!u=_n1ow_hyb')
+    'SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
